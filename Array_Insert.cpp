6dcb5719da2d542c7e6626b58a14f4ae44f314a3
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-09 15:30:50 +0600

    Update Array_Insert.cpp

--- Array_Insert.cpp
+++ Array_Insert.cpp
@@ -1,17 +1,15 @@
 #include<iostream>
 using namespace std;
-int Index; //Made global so it can be accessed from other functions
+int Index; 
 
-// This function does the insertion process
+
 void insert(int *p,int position,int number){
     int i;
     ///Shifting elements by one index ahead
     for(i=Index;i>=position;i-- ){
         p[i] = p[i-1];
     }
-    // After it reached the index it enters the value to that index
     p[position-1] = number;
-    // Incrementing index cause the elements increased by 1
     Index++;
 }
 
