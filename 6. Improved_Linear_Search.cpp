8b55feedeb6b334fcb87acae30cf155ab60a2465
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-10 10:47:54 +0600

    Update 6. Improved_Linear_Search.cpp

--- 6. Improved_Linear_Search.cpp
+++ 6. Improved_Linear_Search.cpp
@@ -3,5 +3,15 @@ using namespace std;
 int n;
 
 int main(){
+     int key;
+    int arr[10] = {10,20,30,40,50,60,70,80,90,100};
+    n = sizeof(arr) / sizeof(int);
+    cout << "Enter an element to search: ";
+    cin >> key;
 
+    int index = LinearSearch(arr,key);
+    if(index == -1)
+        cout << key << " " << "is not present in the array!!" << endl;
+    else
+        cout << key << " " << "Found at index " << index << endl;
 }
