322f78950c9d7544a672f950a13d3634b7fe8628
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-12 11:12:37 +0600

    Update 13.Insert_SortedArray.cpp

--- 13.Insert_SortedArray.cpp
+++ 13.Insert_SortedArray.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+void insert(int *p,int n,int key){
+    while(key < p[n-1]){
+        p[n] = p[n-1];
+        n--;
+    }
+    p[n] = key;
+}
+
 int main(){
     int i;
-    int Arr[10] ={1,2,3,4,5,6,7,8,9,10};
+    int Arr[] ={12,18,24,27,31,39,45,67,71};
     int n = sizeof(Arr) / sizeof(int);
 
     cout << "Before Inserting ";
@@ -11,7 +19,7 @@ int main(){
         cout << Arr[i] << " ";
         cout << endl;
 
-
+    insert(Arr,n,35);
 
 
     cout << "After Inserting ";
