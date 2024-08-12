65c0a25ddb2fbc32b61b9ad46c17235d8f362034
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-12 11:05:53 +0600

    Update 13.Insert_SortedArray.cpp

--- 13.Insert_SortedArray.cpp
+++ 13.Insert_SortedArray.cpp
@@ -6,7 +6,7 @@ int main(){
     int Arr[10] ={1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(Arr) / sizeof(int);
 
-    cout << "Before Shifting ";
+    cout << "Before Inserting ";
     for(i=0;i<n;i++)
         cout << Arr[i] << " ";
         cout << endl;
@@ -14,7 +14,7 @@ int main(){
 
 
 
-    cout << "After Shifting ";
+    cout << "After Inserting ";
     for(i=0;i<n;i++)
         cout << Arr[i] << " ";
         cout << endl;
