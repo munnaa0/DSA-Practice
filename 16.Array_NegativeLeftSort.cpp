ebece4aed03290aaddff1a114e416f97daccf93b
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-14 05:10:22 +0600

    Update 16.Array_NegativeLeftSort.cpp

--- 16.Array_NegativeLeftSort.cpp
+++ 16.Array_NegativeLeftSort.cpp
@@ -6,3 +6,24 @@ void display(int *Arr,int length){
         cout << Arr[i] << " ";
     cout << endl;
 }
+
+int main() {
+    int i,result;
+
+    int Arr[] = {12, 18, 24, 27, 31, 39, 45, 41 ,67, 71};
+    int length = sizeof(Arr)/sizeof(int);
+
+    result = isSorted(Arr, length);
+
+    if(result == 0){
+        cout << "Array is not Sorted !!!" << endl;
+        display(Arr,length);
+    }
+    else{
+        cout << "Array is Sorted !!!" << endl;
+        display(Arr,length);
+    }
+    
+
+    return 0;
+}
