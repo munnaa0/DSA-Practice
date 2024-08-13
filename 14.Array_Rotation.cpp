ee753896c262fda8c0f2246c0bff1a6feb0c95a5
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-13 14:53:59 +0600

    Update 14.Array_Rotation.cpp

--- 14.Array_Rotation.cpp
+++ 14.Array_Rotation.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 void LeftRotate(int *p,int n){
-    int i,temp;
+    int i,temp = p[0];
     for(i=0;i<n-1;i++){
         p[i] = p[i+1];
     }
-    p[n-1] = 0;
+    p[n-1] = temp;
 }
 
 void RightRotate(int *p,int n){
-    int i,temp;
+    int i,temp = p[n-1];
     for(i=n-1;i>0;i--){
         p[i] = p[i-1];
     }
-    p[0] = 0;
+    p[0] = temp;
 }
 
 int main(){
@@ -29,7 +29,7 @@ int main(){
 
 
     //LeftRotate(Arr,n);
-    RightRotate(Arr,n);
+    //RightRotate(Arr,n);
 
     cout << "After Rotating ";
     for(i=0;i<n;i++)
