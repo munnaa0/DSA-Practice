2f28ebd7f198cbf0d89003ed6ffc959cf0c01dd1
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-13 14:48:53 +0600

    Update 14.Array_Rotation.cpp

--- 14.Array_Rotation.cpp
+++ 14.Array_Rotation.cpp
@@ -1,2 +1,24 @@
 #include<iostream>
 using namespace std;
+
+
+
+int main(){
+    int i;
+    int Arr[10] ={1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(Arr) / sizeof(int);
+
+    cout << "Before Shifting ";
+    for(i=0;i<n;i++)
+        cout << Arr[i] << " ";
+        cout << endl;
+
+
+    //LeftShift(Arr,n);
+    RightShift(Arr,n);
+
+    cout << "After Shifting ";
+    for(i=0;i<n;i++)
+        cout << Arr[i] << " ";
+        cout << endl;
+}
