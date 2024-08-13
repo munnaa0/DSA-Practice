927163ab00b95bf89498c228f24a50088de4e319
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-13 14:51:47 +0600

    Update 14.Array_Rotation.cpp

--- 14.Array_Rotation.cpp
+++ 14.Array_Rotation.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
+void LeftShift(int *p,int n){
+    int i,temp;
+    for(i=0;i<n-1;i++){
+        p[i] = p[i+1];
+    }
+    p[n-1] = 0;
+}
 
+void RightShift(int *p,int n){
+    int i,temp;
+    for(i=n-1;i>0;i--){
+        p[i] = p[i-1];
+    }
+    p[0] = 0;
+}
 
 int main(){
     int i;
