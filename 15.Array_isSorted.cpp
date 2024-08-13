56140a0984e18c9965eb3f449183354bdeb9bc40
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-14 05:06:53 +0600

    Update 15.Array_isSorted.cpp

--- 15.Array_isSorted.cpp
+++ 15.Array_isSorted.cpp
@@ -9,7 +9,7 @@ void display(int *Arr,int length){
 
 int isSorted(int *p,int length){
 
-    for(int i = 0; i<length; i++){
+    for(int i = 0; i<length-1; i++){
         if(p[i] > p[i+1])
             return 0;
     }
@@ -19,16 +19,20 @@ int isSorted(int *p,int length){
 int main() {
     int i,result;
 
-    int Arr[] = {12, 18, 24, 27, 31, 39, 45, 67, 71};
+    int Arr[] = {12, 18, 24, 27, 31, 39, 45, 41 ,67, 71};
     int length = sizeof(Arr)/sizeof(int);
-
-    cout << "Before Inserting: ";
-    display(Arr,length);
 
     result = isSorted(Arr, length);
 
-    cout << "After Inserting: ";
-    display(Arr,length);
+    if(result == 0){
+        cout << "Array is not Sorted !!!" << endl;
+        display(Arr,length);
+    }
+    else{
+        cout << "Array is Sorted !!!" << endl;
+        display(Arr,length);
+    }
+    
 
     return 0;
 }
