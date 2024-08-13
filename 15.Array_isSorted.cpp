a68f113029f5a6033dd76c2636760fea25b88f73
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-14 04:52:45 +0600

    Update 15.Array_isSorted.cpp

--- 15.Array_isSorted.cpp
+++ 15.Array_isSorted.cpp
@@ -3,3 +3,29 @@ using namespace std;
 
 
 
+int main() {
+    int i;
+    int *Arr = new int[15]; 
+
+    int initialArr[] = {12, 18, 24, 27, 31, 39, 45, 67, 71};
+    int length = 9;
+
+    for (i = 0; i < length; i++) {
+        Arr[i] = initialArr[i];
+    }
+
+    cout << "Before Inserting: ";
+    for (i = 0; i < length; i++)
+        cout << Arr[i] << " ";
+    cout << endl;
+
+    length = insert(Arr, length, 35);
+
+    cout << "After Inserting: ";
+    for (i = 0; i < length; i++)
+        cout << Arr[i] << " ";
+    cout << endl;
+
+    delete[] Arr;
+    return 0;
+}
