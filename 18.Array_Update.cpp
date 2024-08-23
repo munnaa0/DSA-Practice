5068eb2d300cbb61fe52205088590a69f946a65d
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-23 21:16:26 +0600

    Update 18.Array_Update.cpp

--- 18.Array_Update.cpp
+++ 18.Array_Update.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    
+
      int i,k,Size,item;
     cout << "Enter Array Size: ";
     cin >> Size;
@@ -14,7 +14,7 @@ int main(){
         cin >>Arr[i];
     }
 
-     cout << "Enter the position you want to Update: ";
+    cout << "Enter the position you want to Update: ";
     cin >> k;
 
     if(k<=0 || k>Size-1)
